f7f863399607dc06434f4560156576e507bf19ac
Aditya Purwa <adityapurwa@windowslive.com>
2020-12-26 18:12:34 +0700

    fix config

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,17 +1,19 @@
 import "reflect-metadata";
-import {Connection, createConnection} from "typeorm";
+import { Connection, createConnection } from "typeorm";
 
-import * as config from 'config'
+import * as config from "config";
+
+console.log(config.get("orm"));
 
 export default function initializeDatabase(): Promise<Connection> {
-    return new Promise((resolve, reject) => {
-        createConnection(config.get('orm')).then(async connection => {
-            if(process.env.NODE_ENV === 'test'){
-                await connection.synchronize(true)
-            }
-            resolve(connection)
-        }).catch(error => reject(error));
-    })
+  return new Promise((resolve, reject) => {
+    createConnection(config.get("orm"))
+      .then(async (connection) => {
+        if (process.env.NODE_ENV === "test") {
+          await connection.synchronize(true);
+        }
+        resolve(connection);
+      })
+      .catch((error) => reject(error));
+  });
 }
-
-
